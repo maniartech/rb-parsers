9d79e9f7ad77237e4f2d6fdd405a10fde969e1aa
Aamir Maniar <aamironline@gmail.com>
2024-05-30 17:01:57 +0530

    feat: Add CallbackRule to Tokenizer

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -8,5 +8,6 @@ pub mod symbol_rule;
 pub use closure_rule::ClosureRule;
 pub use regex_rule::RegexRule;
 pub use rule::Rule;
+pub use rule_types::CallbackRule;
 pub use rule_types::RuleType;
 pub use symbol_rule::SymbolRule;
