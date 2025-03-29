42bea6f65940a6f46f8aab5cb725829b248d3dbd
Aamir Maniar <aamironline@gmail.com>
2025-03-29 07:21:34 +0530

    Upgraded to monorepo

--- src/scanners/scanner.rs
+++
@@ -1,6 +0,0 @@
-use crate::tokens::Token;
-use crate::tokens::TokenizationError;
-
-pub trait Scanner {
-    fn scan(&self, input: &str) -> Result<Option<Token>, TokenizationError>;
-}
--- src/tokens/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod error;
-pub mod token;
-
-pub use error::TokenizationError;
-pub use token::Token;
